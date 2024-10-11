9d11b996a3d94e01aded09b66f3959d02ed37c93
fat7i-alghareeb <fat7ighareeb@gmail.com>
2024-10-11 17:47:07 +0300

    add the text styles that is used in the app

---
+++ lib/utils/app_text_styles.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+abstract class TextStyles {
+  static const TextStyle w600s40 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 40,
+  );
+
+  static const TextStyle w600s20 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 20,
+  );
+
+  static const TextStyle w600s17 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 17,
+  );
+
+  static const TextStyle w500s16 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 16,
+  );
+
+  static const TextStyle w500s20 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 20,
+  );
+
+  static const TextStyle w500s24 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 24,
+  );
+
+  static const TextStyle w500s17 = TextStyle(
+    fontWeight: FontWeight.w500,
+    fontSize: 17,
+  );
+
+  static const TextStyle w400s16 = TextStyle(
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+  );
+
+  static const TextStyle w600s24 = TextStyle(
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+  );
+}
